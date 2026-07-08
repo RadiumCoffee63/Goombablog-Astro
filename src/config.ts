34d3d261b173630871f9a2aa6d004982ec30fca5
RadiumCoffee63 <radiumcoffee63@gmail.com>
2026-07-08 20:07:18 +0000

    修复已知问题

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 215, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: true,
@@ -32,7 +32,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave this array empty to use the default favicon
 		 {
-		   src: 'public/favicon/icon.jpg',    // Path of the favicon, relative to the /public directory
+		   src: '/public/favicon/icon.jpg',    // Path of the favicon, relative to the /public directory
 		   theme: 'dark',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		 }
